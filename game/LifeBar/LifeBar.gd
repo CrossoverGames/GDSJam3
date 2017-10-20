46895855668cf72bc1cb45127973151412ae589a
Marcelo Vasques <msouvas@gmail.com>
2017-10-20 16:08:54 -0200

    Updated lion hp functions

--- game/LifeBar/LifeBar.gd
+++ game/LifeBar/LifeBar.gd
@@ -4,3 +4,6 @@ export(float) var max_hp = 100
 
 func _ready():
 	set_max(max_hp)
+	
+func upate_max_hp(value):
+	set_max(value)
--- game/Lion/Lion.gd
+++ game/Lion/Lion.gd
@@ -4,11 +4,27 @@ export(int) var hp = 100
 export(int) var speed = 40
 
 var path_follow
+var lifebar
+
+signal lion_dead
 
 func _ready():
 	path_follow = get_parent()
+	lifebar = get_node("LifeBar")
+	
+	lifebar.uodate_max_hp(hp)
+	
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	var offset = path_follow.get_offset()
 	path_follow.set_offset(offset + (speed * delta))
+	
+func hit(value):
+	if hp > 0:
+		hp -= value
+		lifebar.set_value(hp)
+	else:
+		emit_signal("lion_dead")
+		
+	
